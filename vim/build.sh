c426508ace879347bf310772bb21ddc6d9006b12
John Higgins <john.higgins@eigentech.com>
2018-05-13 06:23:33 +0100

    Added python3

--- vim/build.sh
+++ vim/build.sh
@@ -5,7 +5,10 @@ set -x -e
 INCLUDE_PATH="${PREFIX}/include"
 LIBRARY_PATH="${PREFIX}/lib"
 
-./configure --with-features=huge --prefix=${PREFIX}
+./configure --with-features=huge \
+    --enable-pythoninterp=yes \
+    --enable-python3interp=yes \
+    --prefix=${PREFIX}
 make -j $(nproc)
 make install
 
